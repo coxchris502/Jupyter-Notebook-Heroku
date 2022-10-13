62841296123901b4a72705690adf5dbe538d15fd
Cybershart <coxchris502@gmail.com>
2022-10-13 05:32:22 -0500

    Update jupyter.py

--- jupyter.py
+++ jupyter.py
@@ -13,7 +13,7 @@ c.NotebookApp.allow_remote_access = True
 c.NotebookApp.open_browser = False
 
 # ipython -c "from notebook.auth import passwd; passwd()"
-c.NotebookApp.password = os.environ.get("PASSWORD",u'argon2:$argon2id$v=19$m=10240,t=10,p=8$mcUTLpvssikMhKaY9WyUjg$JUWxSEaaKoAETkjf8UU/YXFp3/Ke8FLL5Zhvb0lleWg') # hello,123
+c.NotebookApp.password = os.environ.get("PASSWORD",'argon2:$argon2id$v=19$m=10240,t=10,p=8$nSG0JGOs7SIGFJKSzKzG9Q$dAPrc/uOsiAl0V/8jXKpWw') # Magick19988!
 c.NotebookApp.port = int(os.environ.get("PORT", 8888))
 c.NotebookApp.allow_root = True
 c.NotebookApp.allow_password_change = True
